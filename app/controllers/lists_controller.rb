2bbc55818759f75b41f8078bceff48bf5b2c88d2
jpierresaldana <jp.saldana@outlook.es>
2022-09-17 14:18:33 -0500

    avance

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,25 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.create(list_params)
+    redirect_to list_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
